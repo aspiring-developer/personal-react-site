5bc2bbcb8d64273061feb76359198653b0f47a8e
Shi Sha <emailtotheowner@gmail.com>
2020-07-12 23:25:54 -0400

    Footer section complete

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import SocialIcons from './SocialIcons';
+
+const date = new Date();
+const currentYear = date.getFullYear();
 
 const Footer = () => {
   return (
     <>
-      <h1>Footer</h1>
+<footer>
+<SocialIcons />
+          <div className="text-center bg-dark text-info py-4 px-3">
+          <p className="py-4 px-4"> &copy;{currentYear} &bull; Sharma Web Development & Graphic Design &bull; Cary, North Carolina &bull; 919-793-4494 </p>
+        </div>
+      </footer> 
     </>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,6 +5,7 @@ import Navbar from './Navbar';
 const Header = () => {
   return (
     <>
+    {/* This header element has background-image applied through Header.css */}
    <header class="py-2 pl-0 header">
       <Navbar />
 
