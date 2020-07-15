7bae8e43b5ef9f90ace512fb8806f3f752b8d15e
Shi Sha <emailtotheowner@gmail.com>
2020-07-15 09:34:15 -0400

    Netlify link fixed

--- src/components/homeContents/HomeMidSection.jsx
+++ src/components/homeContents/HomeMidSection.jsx
@@ -10,10 +10,12 @@ const HomeMidSection = () => {
         <h6 className="text-white p-3" id="greetingTexts"> Welcome! And, thank you for visiting us! </h6>
         <h6 className="text-white" id="greetingTexts"> This website is to showcase some of our work. </h6>
         <p className="text-warning pb-3 py-2"> Click a button below to go to the related portfolio page. </p>
-        <a href="/web-development"> <button type="button" className="btn btn-danger my-1" id="frontLgBtn"> Web Development </button> </a>
+        <Link to="/web-development"> <button type="button" className="btn btn-danger my-1" id="frontLgBtn"> Web Development </button> </Link>
+
         <li className="dropdown-item">
           <Link className="nav-link text-info" to="web-development"><i className="fas fa-laptop-code"></i> Web Development</Link>
         </li>
+
         <a href="/graphic-digital"> <button type="button" className="btn btn-success my-1" id="frontLgBtn"> Graphic Design </button> </a>
         <a href="/graphic-digital"> <button type="button" className="btn btn-primary my-1" id="frontLgBtn"> Digital Drawing </button> </a>
         <a href="/painting"> <button type="button" className="btn btn-light my-1" id="frontLgBtn"> Oil Painting </button> </a>
