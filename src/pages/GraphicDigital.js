70c1b0c4f0469a8ecb154915f27c539e7e94d83a
Shi Sha <emailtotheowner@gmail.com>
2020-09-12 12:39:46 -0400

    Test Component Created

--- src/pages/GraphicDigital.js
+++ src/pages/GraphicDigital.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import Header from '../components/header/Header';
-import Footer from '../components/footer/Footer';
-import GraphicDesignRow from '../components/graphicDigitalContents/GraphicDesignRow';
-import DigitalDrawingRow1 from '../components/graphicDigitalContents/DigitalDrawingRow1';
-
-
-
+import React from "react";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import GraphicDesignRow from "../components/graphicDigitalContents/GraphicDesignRow";
+import DigitalDrawingRow1 from "../components/graphicDigitalContents/DigitalDrawingRow1";
 
 const GraphicDigital = () => {
   return (
     <>
       <Header />
-      <h3 className="text-danger text-center my-5">Graphic Design and Digital Drawing </h3>
+      <h3 className="text-danger text-center my-5">
+        Graphic Design and Digital Drawing{" "}
+      </h3>
       <GraphicDesignRow />
 
       <DigitalDrawingRow1 />
       <Footer />
     </>
-  )
-}
+  );
+};
 export default GraphicDigital;
