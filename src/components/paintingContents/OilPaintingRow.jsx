e0c0f258d05a8e8c91cebf7740057a2f13e48646
Shi Sha <emailtotheowner@gmail.com>
2020-09-18 06:19:55 -0400

    Painting list update.

--- src/components/paintingContents/OilPaintingRow.jsx
+++ src/components/paintingContents/OilPaintingRow.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import PaintingProps from './PaintingProps';
-import './Painting.css';
+import React from "react";
+import PaintingProps from "./PaintingProps";
+import "./Painting.css";
 
 const OilPaintingRow = (props) => {
   return (
@@ -9,7 +9,6 @@ const OilPaintingRow = (props) => {
       <h3 className="text-info text-center mb-4">Oil Paintings </h3>
 
       <div class="row portfolioDisplayTopRow mx-auto d-flex justify-content-center align-items-center">
-
         <PaintingProps
           image="../images/painting-abandoned-boats-300x224.jpg"
           title='Oil Painting: Title "Abandoned"'
@@ -23,43 +22,42 @@ const OilPaintingRow = (props) => {
         <PaintingProps
           image="../images/painting-cary-park-300x224.jpg"
           title='Oil Painting: Title "Cary Park"'
-          description='This is Cary Downtown Park Water Fountain painting. I made several pieces of this kind of oil painting and sold them. Some pieces are still in stock. The size of the painting ranges from 12in x 18in to 30in x 40in.'
+          description="This is Cary Downtown Park Water Fountain painting. I made several pieces of this kind of oil painting and sold them. Some pieces are still in stock. The size of the painting ranges from 12in x 18in to 30in x 40in."
         />
         <PaintingProps
           image="../images/painting-peaceful-countryside-300x224.jpg"
           title='Oil Painting: Title "Country Side Peace"'
-          description='This is 18in x 24in oil painting, made with wet-on-wet and wet-on-dry techniques.'
+          description="This is 18in x 24in oil painting, made with wet-on-wet and wet-on-dry techniques. This is currently in the gallery display."
         />
 
         <PaintingProps
           image="../images/painting-crabtree-park1-300x224.jpg"
           title='Oil Painting: Title "Crabtree Trail "'
-          description='This is 14in x 18in oil painting, made with oil over acrylic technique.'
+          description="This is 14in x 18in oil painting, made with oil over acrylic technique. This painting was sold from Cary Gallery of Artists along with another Crabtree Park Trail version #2."
         />
         <PaintingProps
           image="../images/painting-fishtail-view-300x224.jpg"
           title='Oil Painting: Title "A Beauty"'
-          description='This is 12in x 18in oil painting, made with oil over acrylic technique.'
+          description="This is 12in x 18in oil painting, made with oil over acrylic technique."
         />
         <PaintingProps
           image="../images/painting-stony-lake-300x224.jpg"
           title='Oil Painting: Title "Stony Lake"'
-          description='This is 18in x 24in oil painting, made with wet-on-wet and wet-on-dry techniques.'
+          description="This is 18in x 24in oil painting, made with wet-on-wet and wet-on-dry techniques."
         />
 
         <PaintingProps
           image="../images/painting-sunset-lake-300x224.jpg"
           title='Oil Painting: Title "Sunset Lake"'
-          description='This is 18in x 24in oil painting, made with wet-on-wet and wet-on-dry techniques.'
+          description="This is 18in x 24in oil painting, made with wet-on-wet and wet-on-dry techniques. This is currently in the gallery display."
         />
         <PaintingProps
           image="../images/painting-rupse-300x224.jpg"
           title='Oil Painting: Title "Just Flowing"'
-          description='This is 12in x 18in oil painting, made with oil over acrylic technique.'
+          description="This is 12in x 18in oil painting, made with oil over acrylic technique."
         />
-
       </div>
     </>
-  )
-}
+  );
+};
 export default OilPaintingRow;
