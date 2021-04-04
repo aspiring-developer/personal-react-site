5fa7f3c1b83f7c0f91bd8d69e4b7560d43154b52
ssh1sharma <emailtotheowner@gmail.com>
2021-04-04 12:33:33 -0400

    Site content in graphic design update

--- src/components/graphicDigitalContents/GraphicDesignRow.jsx
+++ src/components/graphicDigitalContents/GraphicDesignRow.jsx
@@ -14,21 +14,22 @@ const GraphicDesignRow = (props) => {
           image="../images/magazine-cover-300x393.jpg"
           alt="Fashion magazine cover design"
           title="Fashion Magazine Cover"
-          description="Fashion magazine cover design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita aliquam eligendi repudiandae dolorem sed illum repellat. Quasi, nemo?"
+          description="This Fashion Magazine Cover design was created as a project while I was studying Advertising and Graphic Design."
         />
         <GraphicDigitalProps
           image="../images/restaurant-menu-300x393.jpg"
           alt="Restaurant menu brochure design"
           title="Restaurant Menu Brochure"
-          description="Brochure design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita aliquam eligendi repudiandae dolorem sed illum repellat. Quasi, nemo? Quasi, nemo?"
+          description="This brochure was designed for a real client. It was a tri-folded brochure to distribute to their takeout-order customers."
         />
         <GraphicDigitalProps
           image="../images/food-magazine-cover-300x393.jpg"
           alt="Food magazine cover design"
           title="Health Magazine Cover"
-          description="Health magazine cover design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita aliquam eligendi repudiandae dolorem sed illum repellat. Quasi, nemo?"
+          description="This Health Magazine Cover design was created as a project while I was studying Advertising and Graphic Design."
         />
-
+<h4 className="magazineInfo px-4 text-center"> <a href="../images/SANGALO-2020-for-site-WEB-VERSION.pdf"> Click here to view one of my client's latest Magazine/Book Design project in PDF version. </a> <br/> <p>Note: It is optimized for web, so quality may have been reduced.</p></h4>
+<br />
       </div>
     </>
   )
